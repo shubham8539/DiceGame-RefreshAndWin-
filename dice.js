ebf9bc37a15462021388a366963046080f91d3ff
Shubham <48403811+shubham8539@users.noreply.github.com>
2020-07-16 22:44:35 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,15 @@
+
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDice1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomDice1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDice2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomDice2);
+
+if (randomNumber1 > randomNumber2) {
+document.querySelector("h1").innerHTML="🥇player1 wins"
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML= "🥇player2 Wins ";
+} else { document.querySelector("h1").innerHTML="🤦‍♂️Draw";}
